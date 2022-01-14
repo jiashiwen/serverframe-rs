4296a5048387101eceb2ef1dfca4fb5b9e5b24dc
jiashiwen <jiashiwen@ZBMac-C02F34Q9MD6M.local>
2022-01-14 15:19:33 +0800

    remove ssh crate

--- src/httpserver/routers/root.rs
+++ src/httpserver/routers/root.rs
@@ -3,7 +3,9 @@ use axum::routing::{get, post};
 use axum::Router;
 
 pub fn router_root() -> Router {
-    let root = Router::new().route("/health", get(root));
+    let root = Router::new()
+        .route("/health", get(root))
+        .route("/health", post(root));
     let api = Router::new().route("/api/tpost", post(tpost));
     return root.merge(api);
 }
