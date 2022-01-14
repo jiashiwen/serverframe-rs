cd538fb78743ad682e6cf2deeaef6146a3fd04b8
jiashiwen <jiashiwen@ZBMac-C02F34Q9MD6M.local>
2022-01-14 15:55:58 +0800

    remove ssh crate

--- src/httpserver/routers/root.rs
+++ src/httpserver/routers/root.rs
@@ -1,9 +1,21 @@
 use crate::httpserver::service::{root, tpost};
 use axum::routing::{get, post};
 use axum::Router;
+use std::time::Duration;
 
 pub fn router_root() -> Router {
+    let middleware_stack = ServiceBuilder::new()
+        // timeout all requests after 10 seconds
+        .timeout(Duration::from_secs(10))
+        // add high level tracing of requests and responses
+        .layer(TraceLayer::new_for_http())
+        // compression responses
+        .layer(CompressionLayer::new())
+        // convert the `ServiceBuilder` into a `tower::Layer`
+        .into_inner();
+
     let root = Router::new()
+        .layer(middleware_stack)
         .route("/health", get(root))
         .route("/health", post(root));
     let api = Router::new().route("/api/tpost", post(tpost));
